c5fa5cce13444fee7031d5f2a46d2a890e25f03a
Renato Cardoso Alves <renatocardosoalves.dev@gmail.com>
2022-11-06 17:33:57 -0300

    aula 2.1 - usando jakarta persistence (JPA)

--- algalog-api/src/main/java/com/github/renatocardosoalves/algalog/controller/ClienteController.java
+++ algalog-api/src/main/java/com/github/renatocardosoalves/algalog/controller/ClienteController.java
@@ -1,32 +1,23 @@
 package com.github.renatocardosoalves.algalog.controller;
 
 import com.github.renatocardosoalves.algalog.domain.model.Cliente;
-import net.datafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        var faker = new Faker();
-
-        var cliente1 = new Cliente();
-        cliente1.setClienteId(1L);
-        cliente1.setNome(faker.name().fullName());
-        cliente1.setTelefone(faker.phoneNumber().phoneNumber());
-        cliente1.setEmail(faker.internet().emailAddress());
-
-        var cliente2 = new Cliente();
-        cliente2.setClienteId(2L);
-        cliente2.setNome(faker.name().fullName());
-        cliente2.setTelefone(faker.phoneNumber().phoneNumber());
-        cliente2.setEmail(faker.internet().emailAddress());
-
-        return List.of(cliente1, cliente2);
+        return this.manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 
 }
--- algalog-api/src/main/java/com/github/renatocardosoalves/algalog/domain/model/Cliente.java
+++ algalog-api/src/main/java/com/github/renatocardosoalves/algalog/domain/model/Cliente.java
@@ -1,12 +1,23 @@
 package com.github.renatocardosoalves.algalog.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long clienteId;
     private String nome;
     private String email;
