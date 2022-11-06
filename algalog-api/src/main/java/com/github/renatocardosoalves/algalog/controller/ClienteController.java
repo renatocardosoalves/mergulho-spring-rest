0ed12d955b0024c32cf8c7c8326b281ba09bf277
Renato Cardoso Alves <renatocardosoalves.dev@gmail.com>
2022-11-06 18:22:42 -0300

    aula 2.4 - validando com bean validation

--- algalog-api/src/main/java/com/github/renatocardosoalves/algalog/controller/ClienteController.java
+++ algalog-api/src/main/java/com/github/renatocardosoalves/algalog/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -30,12 +31,12 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente adicionar(@RequestBody Cliente cliente) {
+    public Cliente adicionar(@RequestBody @Valid Cliente cliente) {
         return this.clienteRepository.save(cliente);
     }
 
     @PutMapping("/{clienteId}")
-    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody @Valid Cliente cliente) {
         if (!this.clienteRepository.existsById(clienteId)) {
             return ResponseEntity.notFound().build();
         }
