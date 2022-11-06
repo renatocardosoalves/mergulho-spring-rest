61a8186e26894e8c56b667b5214da8e493775622
Renato Cardoso Alves <renatocardosoalves.dev@gmail.com>
2022-11-06 17:17:36 -0300

    aula 2.0 - configurando o flyway

---
+++ algalog-api/src/main/resources/db/migration/V001__cria-tabela-cliente.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cliente (
+    cliente_id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    PRIMARY KEY (cliente_id)
+);
